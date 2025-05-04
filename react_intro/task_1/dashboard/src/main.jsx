331cf30ae2ca40577303d7144ba5670cd1b6a6d2
Yassinelaz1 <yassinelazhari524@gmail.com>
2025-05-04 17:14:17 +0100

    update

--- react_intro/task_1/dashboard/src/main.jsx
+++ react_intro/task_1/dashboard/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Notifications from "./Notifications";
 
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+  document.getElementById("root")
+);
