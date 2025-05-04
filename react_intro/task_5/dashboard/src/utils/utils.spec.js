fa6d24172c8e257e8d9c82854d5c63c8c7807a3f
Yassinelaz1 <yassinelazhari524@gmail.com>
2025-05-04 22:00:05 +0100

    update

--- react_intro/task_5/dashboard/src/utils/utils.spec.js
+++ react_intro/task_5/dashboard/src/utils/utils.spec.js
@@ -1,6 +1,5 @@
 // task_3/dashboard/src/utils.spec.js
-
-import { getCurrentYear, getFooterCopy, getLatestNotification } from './utils/utils';
+import { getCurrentYear, getFooterCopy, getLatestNotification } from './utils';
 
 describe('Utils functions', () => {
   test('getCurrentYear returns the current year', () => {
